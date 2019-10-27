25484fc1654148d43872d124bf41fd7ba7cc0673
Michaelz Omoakin <akindiileteforex@gmail.com>
2019-10-27 11:41:05 +0100

    db adjust

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,17 +2,20 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/cashbox-api", {
+/* mongoose.connect("mongodb://127.0.0.1:27017/cashbox-api", {
   useNewUrlParser: true,
   //ds facilitates mongoose-mongodb connection so we can quickly access d data we want to access.
   useCreateIndex: true,
   //  correcting findAndModify deprecation warning
   useFindAndModify: false
-});
-/*mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  //ds facilitates mongoose-mongodb connection so we can quickly access d data we want to access.
-  useCreateIndex: true,
-  //  correcting findAndModify deprecation warning
-  useFindAndModify: false
-});*/
+}); */
+mongoose.connect(
+  "mongodb+srv://michaelztaskapp:123999abc@cluster0-4sjnt.mongodb.net/cashbox-api",
+  {
+    useNewUrlParser: true,
+    //ds facilitates mongoose-mongodb connection so we can quickly access d data we want to access.
+    useCreateIndex: true,
+    //  correcting findAndModify deprecation warning
+    useFindAndModify: false
+  }
+);
